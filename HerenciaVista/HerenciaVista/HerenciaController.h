1731854d49224d1d45fb7c28bdcd96c8f92b175e
Henry AT <yiperu@gmail.com>
2013-09-24 22:18:23 -0500

    Creando el controlados de Herencia que sera Control Main Principal

---
+++ HerenciaVista/HerenciaVista/HerenciaController.h
@@ -0,0 +1,13 @@
+//
+//  HerenciaController.h
+//  HerenciaVista
+//
+//  Created by Henry AT on 9/24/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HerenciaController : UIViewController
+
+@end
