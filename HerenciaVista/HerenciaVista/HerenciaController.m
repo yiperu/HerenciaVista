e9fa47cc68d93512dac0612882d06552a493dd9b
Henry AT <yiperu@gmail.com>
2013-09-24 22:49:52 -0500

    Personalizacion de mainView principal

--- HerenciaVista/HerenciaVista/HerenciaController.m
+++ HerenciaVista/HerenciaVista/HerenciaController.m
@@ -14,11 +14,37 @@
 
 @implementation HerenciaController
 
+@synthesize mainView, view1, view2;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        // Personalizacion de la inicializacion
+        mainView = [[MainView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+        
+        // establecer el lbackground coloe del mainView
+        mainView.backgroundColor = [UIColor greenColor];
+        
+        //Establecer como el controlador principal
+        self.view = mainView;
+        
+        //Make subview rectagles, en contexto del mainView's frame
+        CGRect view1Frame = CGRectMake(50, 50, 100, 100);
+        CGRect view2Frame = CGRectMake(100, 100, 100, 100);
+        
+        //Instanciar las subvistas
+        view1 = [[View1 alloc] initWithFrame:view1Frame];
+        view2 = [[View2 alloc] initWithFrame:view2Frame];
+        
+        //Establecer algunas propiedades
+        view1.backgroundColor = [UIColor redColor];
+        view2.backgroundColor = [UIColor blueColor];
+        
+        // Adicionando las 2 Subvistas al view controller
+        [self.view addSubview:view1];
+        [self.view addSubview:view2];
+        
     }
     return self;
 }
